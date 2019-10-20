52b9bf4b739b7bdbc48007aeaa17721ba2250710
Rodrigo Schemes Bitencourt <rodrigoschemes@yahoo.com.br>
2019-10-20 12:45:43 -0300

    complete

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -3,22 +3,17 @@ import utils from '../Utils/utils';
 import PlayNumber from './PlayNumber';
 import PlayAgain from './PlayAgain';
 import StarsDisplay from './StarsDisplay';
+import UseGameState from './UseGameState';
 
 const Game = ({startNewGame}) => {
-    const [stars, setStars] = useState(utils.random(1, 9))
-    const [availableNums, setAvailableNums] = useState(utils.range(1, 9))
-    const [candidateNums, setCandidateNums] = useState([])
-    const [secondsLeft, setSecondsLeft] = useState(10)
 
-    useEffect(() => {
-        if (secondsLeft > 0 && availableNums.length > 0){
-            const timerId = setTimeout(() => {
-                setSecondsLeft(secondsLeft - 1)
-            }, 1000)
-
-            return () => clearTimeout(timerId)
-        }
-    })
+    const {
+        stars, 
+        availableNums, 
+        candidateNums, 
+        secondsLeft, 
+        setGameState
+    } = UseGameState()
 
     const candidatesAreWrong = utils.sum(candidateNums) > stars;
     const gameStatus = availableNums.length === 0 
@@ -45,16 +40,7 @@ const Game = ({startNewGame}) => {
                 ? candidateNums.concat(number)
                 : candidateNums.filter(cn => cn !== number);
 
-        if (utils.sum(newCandidateNums) !== stars) {
-            setCandidateNums(newCandidateNums);
-        } else {
-            const newAvailableNums = availableNums.filter(
-                n => !newCandidateNums.includes(n)
-            );
-            setStars(utils.randomSumIn(newAvailableNums, 9));
-            setAvailableNums(newAvailableNums);
-            setCandidateNums([]);
-        }
+        setGameState(newCandidateNums)
     };
 
     return (
